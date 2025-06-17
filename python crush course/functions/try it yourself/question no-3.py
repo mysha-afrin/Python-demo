2cf0f1797efe340c34e3da3c226def8c48899904
S.rahman <standerstate098@gmail.com>
2025-06-17 09:55:43 +0600

    Update question no-3.py

--- python crush course/functions/try it yourself/question no-3.py
+++ python crush course/functions/try it yourself/question no-3.py
@@ -4,3 +4,4 @@ def make_shirt(size, message):
     print(f"\nIf the shirt size is {size}, then the message on the shirt will be {message.title()}.")
 make_shirt('large', 'I love Python')
 make_shirt('medium', 'Python is awesome')
+make_shirt('XL',"Hello World Is My First Love") # Please Accpet my order
