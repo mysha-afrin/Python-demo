50cb63bddb6585b279938f27fb3fb564395d890d
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-17 09:31:39 +0600

    Update multiple_function_call.py

--- python crush course/functions/books concept/multiple_function_call.py
+++ python crush course/functions/books concept/multiple_function_call.py
@@ -4,3 +4,12 @@ def pet_animal(animal_type, pet_name):
     print(f"My {animal_type}'s name is {pet_name.title()}.\n")
 pet_animal('dog', 'bruno')
 pet_animal('cat', 'whiskers')
+
+#creating a error by calling the function with different parameters
+
+def pet_animal(animal_type, pet_name):
+    '''Display information about a pet.
+    '''
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.\n")
+pet_animal('bruno', 'dog')
