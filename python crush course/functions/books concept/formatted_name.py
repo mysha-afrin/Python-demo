b0469dcc170180a5fd80f0c89a8cf981d328bef9
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-17 18:10:56 +0600

    Create formatted_name.py

---
+++ python crush course/functions/books concept/formatted_name.py
@@ -0,0 +1,6 @@
+def get_formatted_name(first_name, last_name):
+    """Return a full namel, nearly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+musician = get_formatted_name('jimi', 'hendrik')
+print(musician)
