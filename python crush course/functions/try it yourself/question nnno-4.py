110b424ffefc82e3c9a265ea8251d7483f9d5b16
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-17 09:51:29 +0600

    Create question nnno-4.py

---
+++ python crush course/functions/try it yourself/question nnno-4.py
@@ -0,0 +1,7 @@
+# This code defines a function that prints a message about a shirt size and a message on it.
+# It then calls the function twice with different arguments.
+def make_shirt(size, message):
+    print(f"\nIf the shirt size is {size}, then the message on the shirt will be {message.title()}.\n")
+make_shirt('large', 'I love Python')
+make_shirt('medium', 'Python is awesome')
+make_shirt('small', 'Python is great')
