894566ce5b9b5ad480fa87dbd3451c59d20f44b6
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-20 11:26:28 +0600

    Update multiple_function_call.py

--- python crush course/functions/books concept/multiple_function_call.py
+++ python crush course/functions/books concept/multiple_function_call.py
@@ -13,3 +13,12 @@ def pet_animal(animal_type, pet_name):
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.\n")
 pet_animal('bruno', 'dog')
+
+
+
+def build_person(first_name, last_name):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    return person
+musician = build_person('jimi', 'hendrix')
+print(musician)
