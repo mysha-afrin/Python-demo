a22ae0fe682e808fb6defc9f1c74117c0ffd10b4
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-20 09:00:03 +0600

    Update formatted_name.py

--- python crush course/functions/books concept/formatted_name.py
+++ python crush course/functions/books concept/formatted_name.py
@@ -4,3 +4,12 @@ def get_formatted_name(first_name, last_name):
     return full_name.title()
 musician = get_formatted_name('jimi', 'hendrik')
 print(musician)
+
+
+
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a formatted full name, with an optional middle name."""
+    full_name = f"{first_name} {middle_name} {last_name}".title()
+    return full_name
+musician = get_formatted_name("jimi", "hendrix", "lee")
+print(musician)
