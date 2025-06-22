b1e26b6442005ae2b033b9f6947064fccb4255aa
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-22 08:21:40 +0600

    Update length of parametre.py

--- python crush course/try your self/done by jeba/chapter-5/lecture-6/length of parametre.py
+++ python crush course/try your self/done by jeba/chapter-5/lecture-6/length of parametre.py
@@ -1,1 +1,7 @@
 
+cities = ['dhaka', 'khulna', 'cumilla', 'chottogram']
+countries = ['bangladesh', 'pakistan', 'america', 'canada']
+def print_cities(list):
+    print(len(list))
+print_cities(cities)
+print_cities(countries)
