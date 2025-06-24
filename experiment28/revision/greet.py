a78ff98238033967947ff1b756db55084bac0e72
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-24 12:39:46 +0600

    Update greet.py

--- experiment28/revision/greet.py
+++ experiment28/revision/greet.py
@@ -16,3 +16,9 @@ def greet_user(username):
     print(f"Hello,{username.title()}")
 greet_user("mysha afrin")
 greet_user("jinia afrin")
+#qn no-1 solved
+def display_message(message):
+    print(message)
+display_message("I am learing python")
+
+
