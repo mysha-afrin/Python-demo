6c8d13d05bcda80e02ba3ff78e6d70c3997a773e
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-24 12:37:59 +0600

    Update greet.py

--- experiment28/revision/greet.py
+++ experiment28/revision/greet.py
@@ -8,3 +8,11 @@ greet_user()
 def greet_user(username):
     print(f"Hello,{username.title()}")
 greet_user("jeba")
+
+
+#passing a information in function
+def greet_user(username):
+    """Display a simple greeting ."""
+    print(f"Hello,{username.title()}")
+greet_user("mysha afrin")
+greet_user("jinia afrin")
